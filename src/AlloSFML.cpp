68c26f57706f100e8b1a94dbf1408026fc8703f8
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-29 13:06:05 -0400

    Ronds d'un smiley

--- src/AlloSFML.cpp
+++ src/AlloSFML.cpp
@@ -13,18 +13,31 @@ using namespace sf;
 int main()
 {
     RenderWindow fenetre(VideoMode(LARGEUR, HAUTEUR), "SFML !");
-    CircleShape cercle(100.f);
-    cercle.setFillColor(Color::Green);
     while (fenetre.isOpen())
     {
+        fenetre.clear();
+
         Event evenement;
         while (fenetre.pollEvent(evenement))
         {
             if (evenement.type == Event::Closed)
                 fenetre.close();
         }
-        fenetre.clear();
-        fenetre.draw(cercle);
+
+        CircleShape* visage = new CircleShape(50.0f);
+        (*visage).setFillColor(Color::Yellow);
+        visage->setFillColor(Color::Yellow);
+        visage->setPosition(20,20);
+        CircleShape* oeilDroit = new CircleShape(10.0f);
+        oeilDroit->setFillColor(Color::Green);
+        oeilDroit->setPosition(80,40);
+        CircleShape* oeilGauche = new CircleShape(10.0f);
+        oeilGauche->setFillColor(Color::Green);
+        oeilGauche->setPosition(40,40);
+
+        fenetre.draw(*visage);
+        fenetre.draw(*oeilDroit);
+        fenetre.draw(*oeilGauche);
         fenetre.display();
     }
     return 0;

be70627708822a07f8e7949efbbbf1880500293b
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-29 13:28:29 -0400

    Classe Binette vide

---
+++ src/Binette.cpp
@@ -0,0 +1,21 @@
+/*
+ * Binette.cpp
+ *
+ *  Created on: 29 oct. 2018
+ *      Author: stamandnadine
+ */
+
+#include "Binette.h"
+
+namespace MondeMagique {
+
+Binette::Binette() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Binette::~Binette() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace MondeMagique */
---
+++ src/Binette.h
@@ -0,0 +1,21 @@
+/*
+ * Binette.h
+ *
+ *  Created on: 29 oct. 2018
+ *      Author: stamandnadine
+ */
+
+#ifndef BINETTE_H_
+#define BINETTE_H_
+
+namespace MondeMagique {
+
+class Binette {
+public:
+	Binette();
+	virtual ~Binette();
+};
+
+} /* namespace MondeMagique */
+
+#endif /* BINETTE_H_ */

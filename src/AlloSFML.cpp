35fa60a94f58282cf83e9c8cb4c08dee7a26ed6b
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-29 13:17:30 -0400

    Decalage du rayon

--- src/AlloSFML.cpp
+++ src/AlloSFML.cpp
@@ -24,26 +24,26 @@ int main()
                 fenetre.close();
         }
 
-        float rayon = 25.0f;
+        float rayon = 50.0f;
         float deplacementX = 50.0f;
         float deplacementY = 50.0f;
 
         // https://www.sfml-dev.org/documentation/2.5.0/classsf_1_1CircleShape.php
-        CircleShape* visage = new CircleShape(2*rayon);
+        CircleShape* visage = new CircleShape(rayon);
         (*visage).setFillColor(Color::Yellow);
         visage->setFillColor(Color::Yellow);
-        visage->setPosition(0,0);
+        visage->setPosition(0 - rayon,0 - rayon);
         CircleShape* oeilDroit = new CircleShape(10.0f);
         oeilDroit->setFillColor(Color::Green);
-        oeilDroit->setPosition(60,20);
+        oeilDroit->setPosition(60 - rayon,20 - rayon);
         CircleShape* oeilGauche = new CircleShape(10.0f);
         oeilGauche->setFillColor(Color::Green);
-        oeilGauche->setPosition(20,20);
+        oeilGauche->setPosition(20 - rayon,20 - rayon);
 
         // https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1RectangleShape.php
         RectangleShape* bouche = new RectangleShape(Vector2f(70,5));
         bouche->setFillColor(Color::Red);
-        bouche->setPosition(15,60);
+        bouche->setPosition(15 - rayon,60 - rayon);
 
         fenetre.draw(*visage);
         fenetre.draw(*oeilDroit);

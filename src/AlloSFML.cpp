f6f451cf9e4f1ae21fb27c31b2eec7b7c3c89823
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-29 13:27:47 -0400

    Déclarations séparées des new, pour préparer la transformation en classe.

--- src/AlloSFML.cpp
+++ src/AlloSFML.cpp
@@ -16,20 +16,25 @@ int main()
     const float positionX = 100.0f;
     const float positionY = 100.0f;
 
+    CircleShape* visage;
+    CircleShape* oeilDroit;
+    CircleShape* oeilGauche;
+    RectangleShape* bouche;
+
     // https://www.sfml-dev.org/documentation/2.5.0/classsf_1_1CircleShape.php
-    CircleShape* visage = new CircleShape(rayon);
+    visage = new CircleShape(rayon);
     (*visage).setFillColor(Color::Yellow);
     visage->setFillColor(Color::Yellow);
     visage->setPosition(0 - rayon + positionX,0 - rayon + positionY);
-    CircleShape* oeilDroit = new CircleShape(10.0f);
+    oeilDroit = new CircleShape(10.0f);
     oeilDroit->setFillColor(Color::Green);
     oeilDroit->setPosition(60 - rayon + positionX,20 - rayon + positionY);
-    CircleShape* oeilGauche = new CircleShape(10.0f);
+    oeilGauche = new CircleShape(10.0f);
     oeilGauche->setFillColor(Color::Green);
     oeilGauche->setPosition(20 - rayon + positionX,20 - rayon + positionY);
 
     // https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1RectangleShape.php
-    RectangleShape* bouche = new RectangleShape(Vector2f(70,5));
+    bouche = new RectangleShape(Vector2f(70,5));
     bouche->setFillColor(Color::Red);
     bouche->setPosition(15 - rayon + positionX,60 - rayon + positionY);
 

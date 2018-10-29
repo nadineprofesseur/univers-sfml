60ad7096a3a370ccc4ddbe8e15ff38d8a366a2c6
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-29 13:10:52 -0400

    Bonhomme smiley complet

--- src/AlloSFML.cpp
+++ src/AlloSFML.cpp
@@ -24,6 +24,7 @@ int main()
                 fenetre.close();
         }
 
+        // https://www.sfml-dev.org/documentation/2.5.0/classsf_1_1CircleShape.php
         CircleShape* visage = new CircleShape(50.0f);
         (*visage).setFillColor(Color::Yellow);
         visage->setFillColor(Color::Yellow);
@@ -35,9 +36,16 @@ int main()
         oeilGauche->setFillColor(Color::Green);
         oeilGauche->setPosition(40,40);
 
+        // https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1RectangleShape.php
+        RectangleShape* bouche = new RectangleShape(Vector2f(70,5));
+        bouche->setFillColor(Color::Red);
+        bouche->setPosition(35,80);
+
         fenetre.draw(*visage);
         fenetre.draw(*oeilDroit);
         fenetre.draw(*oeilGauche);
+        fenetre.draw(*bouche);
+
         fenetre.display();
     }
     return 0;

df4253988186868390bf6ef68d1640c81c2fcefb
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-29 13:19:04 -0400

    Smiley positionnable

--- src/AlloSFML.cpp
+++ src/AlloSFML.cpp
@@ -25,25 +25,25 @@ int main()
         }
 
         float rayon = 50.0f;
-        float deplacementX = 50.0f;
-        float deplacementY = 50.0f;
+        float positionX = 100.0f;
+        float positionY = 100.0f;
 
         // https://www.sfml-dev.org/documentation/2.5.0/classsf_1_1CircleShape.php
         CircleShape* visage = new CircleShape(rayon);
         (*visage).setFillColor(Color::Yellow);
         visage->setFillColor(Color::Yellow);
-        visage->setPosition(0 - rayon,0 - rayon);
+        visage->setPosition(0 - rayon + positionX,0 - rayon + positionY);
         CircleShape* oeilDroit = new CircleShape(10.0f);
         oeilDroit->setFillColor(Color::Green);
-        oeilDroit->setPosition(60 - rayon,20 - rayon);
+        oeilDroit->setPosition(60 - rayon + positionX,20 - rayon + positionY);
         CircleShape* oeilGauche = new CircleShape(10.0f);
         oeilGauche->setFillColor(Color::Green);
-        oeilGauche->setPosition(20 - rayon,20 - rayon);
+        oeilGauche->setPosition(20 - rayon + positionX,20 - rayon + positionY);
 
         // https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1RectangleShape.php
         RectangleShape* bouche = new RectangleShape(Vector2f(70,5));
         bouche->setFillColor(Color::Red);
-        bouche->setPosition(15 - rayon,60 - rayon);
+        bouche->setPosition(15 - rayon + positionX,60 - rayon + positionY);
 
         fenetre.draw(*visage);
         fenetre.draw(*oeilDroit);
